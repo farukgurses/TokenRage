9521a982710e1da674b16634b9c12fc74b35ab15
yusufsina <yusufsinayildiz@gmail.com>
2021-11-02 22:39:06 +0300

    seperated frontend-hardhat

---
+++ frontend/src/redux/reducers/user.reducer.ts
@@ -0,0 +1,20 @@
+import { SET_USERNAME } from "../constants";
+
+const initialState: { name: string } = {
+  name: "",
+};
+
+export default (
+  state = initialState,
+  action: { type: string; payload: any }
+) => {
+  switch (action.type) {
+    case SET_USERNAME:
+      return {
+        ...state,
+        name: action.payload,
+      };
+    default:
+      return state;
+  }
+};
